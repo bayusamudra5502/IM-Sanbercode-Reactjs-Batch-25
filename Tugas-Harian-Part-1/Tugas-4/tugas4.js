a6e8241c741b1dbb8b2ee3040a5547af1492c6f3
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-06-10 13:36:41 +0700

    Menambah tugas 4

---
+++ Tugas-Harian-Part-1/Tugas-4/tugas4.js
@@ -0,0 +1,81 @@
+// SOAL 1
+var i = 0;
+console.log("LOOPING PERTAMA");
+
+while (i <= 20) {
+    if (i > 0 && i % 2 === 0) {
+        console.log(i.toString(), "-", "I love coding");
+    }
+
+    i += 1;
+}
+console.log("LOOPING KEDUA");
+
+while (i > 0) {
+    if (i <= 20 && i % 2 === 0) {
+        console.log(i.toString(), "-", "I will become a frontend developer.");
+    }
+
+    i -= 1;
+}
+
+console.log();
+
+// SOAL 2
+for (i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 2 !== 0) {
+        console.log(i.toString(), "-", "I Love Coding");
+    } else if (i % 2 == 0) {
+        console.log(i.toString(), "-", "Berkualitas");
+    } else {
+        console.log(i.toString(), "-", "Santai");
+    }
+}
+console.log();
+
+// SOAL 3
+for (i = 1; i <= 7; i++) {
+    var str = "";
+    for (var j = 0; j < i; j++) {
+        str += "#";
+    }
+
+    console.log(str);
+}
+console.log();
+
+// SOAL 4
+var kalimat = [
+    "aku",
+    "saya",
+    "sangat",
+    "sangat",
+    "senang",
+    "belajar",
+    "javascript",
+];
+
+kalimat.shift();
+kalimat.splice(1, 1);
+console.log('"' + kalimat.join(" ") + '"');
+console.log();
+
+// SOAL 5
+var sayuran = [];
+sayuran.push(
+    "Bayam",
+    "Buncis",
+    "Kangkung",
+    "Kubis",
+    "Seledri",
+    "Tauge",
+    "Timun"
+);
+
+sayuran.sort();
+
+for (i = 0; i < sayuran.length; i++) {
+    console.log((i + 1).toString() + ".", sayuran[i]);
+}
+
+console.log();
