166f29bdf161aac850cab7366f8612856f3c1dce
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-06-15 18:39:27 +0700

    Menggunakan Closure di start

--- Tugas-Harian-Part-1/Tugas-7/tugas7.js
+++ Tugas-Harian-Part-1/Tugas-7/tugas7.js
@@ -89,29 +89,29 @@ class Clock {
         this._template = template;
     }
 
-    render() {
-        var date = new Date();
-
-        var hours = date.getHours();
-        if (hours < 10) hours = "0" + hours;
-
-        var mins = date.getMinutes();
-        if (mins < 10) mins = "0" + mins;
-
-        var secs = date.getSeconds();
-        if (secs < 10) secs = "0" + secs;
-
-        var output = this._template
-            .replace("h", hours)
-            .replace("m", mins)
-            .replace("s", secs);
-
-        console.log(output);
-    }
-
     start() {
-        this.render();
-        this._timer = setInterval(() => this.render(), 1000);
+        const render = () => {
+            var date = new Date();
+
+            var hours = date.getHours();
+            if (hours < 10) hours = "0" + hours;
+
+            var mins = date.getMinutes();
+            if (mins < 10) mins = "0" + mins;
+
+            var secs = date.getSeconds();
+            if (secs < 10) secs = "0" + secs;
+
+            var output = this._template
+                .replace("h", hours)
+                .replace("m", mins)
+                .replace("s", secs);
+
+            console.log(output);
+        };
+
+        render();
+        this._timer = setInterval(render, 1000);
     }
 
     stop() {
