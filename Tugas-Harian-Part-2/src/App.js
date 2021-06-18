1c542530b403dcc1646838bd5c3134475d6732b6
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2021-06-18 15:32:40 +0700

    Menambah tugas 10

--- Tugas-Harian-Part-2/src/App.js
+++ Tugas-Harian-Part-2/src/App.js
@@ -1,70 +1,16 @@
+import React from "react";
+import FormBuah from "./Tugas-9/Tugas9";
+import TabelBuahTerisi from "./Tugas-10/Tugas10";
 import "./App.css";
 
-function App() {
-    return (
-        <div className="form">
-            <h1>Form Pembelian Buah</h1>
-            <form action="#">
-                <p>
-                    <label htmlFor="nama-pelanggan">Nama Pelanggan</label>
-                    <input id="nama-pelanggan" name="nama" required />
-                </p>
-                <p>
-                    <label>Daftar Item</label>
-                    <div className="daftar-buah">
-                        <p className="pilihan-buah">
-                            <input
-                                type="checkbox"
-                                name="buahSemangka"
-                                id="semangka"
-                                value="true"
-                            />
-                            <label htmlFor="semangka">Semangka</label>
-                        </p>
-                        <p className="pilihan-buah">
-                            <input
-                                type="checkbox"
-                                name="buahJeruk"
-                                id="jeruk"
-                                value="true"
-                            />
-                            <label htmlFor="jeruk">Jeruk</label>
-                        </p>
-                        <p className="pilihan-buah">
-                            <input
-                                type="checkbox"
-                                name="buahNanas"
-                                id="nanas"
-                                value="true"
-                            />
-                            <label htmlFor="nanas">Nanas</label>
-                        </p>
-                        <p className="pilihan-buah">
-                            <input
-                                type="checkbox"
-                                name="buahSalak"
-                                id="salak"
-                                value="true"
-                            />
-                            <label htmlFor="salak">Salak</label>
-                        </p>
-                        <p className="pilihan-buah">
-                            <input
-                                type="checkbox"
-                                name="buahAnggur"
-                                id="anggur"
-                                value="true"
-                            />
-                            <label htmlFor="anggur">Anggur</label>
-                        </p>
-                    </div>
-                </p>
-                <p>
-                    <button>Kirim</button>
-                </p>
-            </form>
-        </div>
-    );
-}
 
-export default App;
+export default class App extends React.Component{
+    render(){
+        return (
+            <main>
+                <FormBuah/>
+                <TabelBuahTerisi />
+            </main>
+        )
+    }
+}
---
+++ Tugas-Harian-Part-2/src/Tugas-10/Tugas10.js
@@ -0,0 +1,75 @@
+import React from "react";
+import "./Tugas10.css";
+import PropTypes from "prop-types";
+
+class TabelBuah extends React.Component {
+    render() {
+        const barisData = this.props.dataBuah.map((buah) => (
+            <TabelBuahRow 
+                key={buah.nama} 
+                nama={buah.nama} 
+                harga={buah.harga} 
+                berat={buah.berat} />
+        ));
+
+        return (
+            <>
+                <h2>Tabel Harga Buah</h2>
+                <table>
+                    <colgroup>
+                        <col className="kolom-nama"/>
+                        <col className="kolom-harga"/>
+                        <col className="kolom-berat"/>
+                    </colgroup>
+                    <tr>
+                        <th>Nama</th>
+                        <th>Harga</th>
+                        <th>Berat</th>
+                    </tr>
+                    { barisData }
+                </table>
+            </>
+        );
+    }
+}
+
+class TabelBuahTerisi extends React.Component{
+    render(){
+        const dataHargaBuah = [
+            {nama: "Semangka", harga: 10000, berat: 1000},
+            {nama: "Anggur", harga: 40000, berat: 500},
+            {nama: "Strawberry", harga: 30000, berat: 400},
+            {nama: "Jeruk", harga: 30000, berat: 1000},
+            {nama: "Mangga", harga: 30000, berat: 500}
+          ];
+
+          return (<TabelBuah dataBuah={dataHargaBuah}/>)
+    }
+}
+
+class TabelBuahRow extends React.Component {
+    render() {
+        return (
+            <tr>
+                <td>{this.props.nama}</td>
+                <td>{this.props.harga}</td>
+                <td>{this.props.berat}</td>
+            </tr>
+        );
+    }
+}
+
+TabelBuahRow.propTypes = {
+    nama: PropTypes.string,
+    harga: PropTypes.number,
+    berat: PropTypes.number
+};
+
+TabelBuah.propTypes = {
+    dataBuah: PropTypes.arrayOf(PropTypes.exact(TabelBuahRow.propTypes))
+};
+
+export {
+    TabelBuahTerisi as default,
+    TabelBuah
+}
